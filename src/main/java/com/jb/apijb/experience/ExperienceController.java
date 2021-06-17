189179815a3bd791b19c6c7567508f401407128c
Bretton <jbretton@entoria.fr>
2021-06-17 17:39:54 +0200

    Add put and delete method for experience

--- src/main/java/com/jb/apijb/experience/ExperienceController.java
+++ src/main/java/com/jb/apijb/experience/ExperienceController.java
@@ -3,12 +3,10 @@ package com.jb.apijb.experience;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class ExperienceController {
@@ -36,6 +34,41 @@ public class ExperienceController {
         try {
             experienceService.createExperience(experience);
             return new ResponseEntity<>(HttpStatus.CREATED);
+        } catch (Exception exception) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PutMapping("/experiences/{id}")
+    public ResponseEntity<Experience> updateExperience(@PathVariable("id") long id, @RequestBody Experience experience) {
+        try {
+            Optional<Experience> experienceOptional = experienceService.findExperience(id);
+
+            if (experienceOptional.isPresent()) {
+                Experience _experience = experienceOptional.get();
+                _experience.setJob(experience.getJob());
+                _experience.setCompany(experience.getCompany());
+                _experience.setYearStart(experience.getYearStart());
+                _experience.setYearEnd(experience.getYearEnd());
+                _experience.setLanguages(experience.getLanguages());
+                _experience.setDescription(experience.getDescription());
+
+                Experience updatedExperience = experienceService.updateExperience(_experience);
+
+                return new ResponseEntity<>(updatedExperience, HttpStatus.OK);
+            }
+
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } catch (Exception exception) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @DeleteMapping("/experiences/{id}")
+    public ResponseEntity<HttpStatus> deleteExperience(@PathVariable("id") long id) {
+        try {
+            experienceService.deleteExperience(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         } catch (Exception exception) {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
--- src/main/java/com/jb/apijb/experience/ExperienceService.java
+++ src/main/java/com/jb/apijb/experience/ExperienceService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ExperienceService {
@@ -11,13 +12,23 @@ public class ExperienceService {
     @Autowired
     private ExperienceRepository experienceRepository;
 
-    // Récupère la liste des experiences
     public List<Experience> getAllExperiences() {
         return experienceRepository.findAll();
     }
 
-    // Création d'une experience
     public void createExperience(Experience experience) {
         experienceRepository.save(new Experience(experience.getCompany(), experience.getJob(), experience.getDescription(), experience.getYearStart(), experience.getYearEnd(), experience.getLanguages()));
+    }
+
+    public Experience updateExperience(Experience experience) {
+        return experienceRepository.save(experience);
+    }
+
+    public Optional<Experience> findExperience(long id) {
+        return experienceRepository.findById(id);
+    }
+
+    public void deleteExperience(long id) {
+        experienceRepository.deleteById(id);
     }
 }
