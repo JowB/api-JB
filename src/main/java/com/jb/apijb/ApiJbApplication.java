06cd08bac094ee5001f41fc515deb5abf30c3c2e
Bretton <jbretton@entoria.fr>
2021-07-27 10:07:37 +0200

    Define java version

--- src/main/java/com/jb/apijb/ApiJbApplication.java
+++ src/main/java/com/jb/apijb/ApiJbApplication.java
@@ -9,6 +9,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class ApiJbApplication {
 
 	public static void main(String[] args) {
+		System.setProperty("java.runtime.version", "11");
 		SpringApplication.run(ApiJbApplication.class, args);
 	}
 
