f4f3c6b286547d92f64486166ca5c7be1448bd1d
Bretton <jbretton@entoria.fr>
2021-07-27 16:09:25 +0200

    Add Procfile for heroku

--- src/main/java/com/jb/apijb/ApiJbApplication.java
+++ src/main/java/com/jb/apijb/ApiJbApplication.java
@@ -9,7 +9,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class ApiJbApplication {
 
 	public static void main(String[] args) {
-		System.setProperty("java.runtime.version", "11");
 		SpringApplication.run(ApiJbApplication.class, args);
 	}
 
